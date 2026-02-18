93d756806250b2c2c9981b6133ce7bea44c64f2a
Kartvya bhoir <kartvyabhoir2007@gmail.com>
2026-02-18 14:21:31 +0530

    Delete exp5.py

--- exp5.py
+++
@@ -1,7 +0,0 @@
-# students in exams
-cet = {"Kartavya", "Bob"}
-jee = {"Tathagat", "Bob"}
-neet = {"Saad", "Eve"}
-print("All students:", cet|jee|neet) #Union
-print("Students in all exam:", cet & jee & neet) # intersection
-print("Cet but not in jee:", cet - jee) #Difference
