a3ff332010cb7d97ed9cd408ed8cfd38205cb353
Kartvya bhoir <kartvyabhoir2007@gmail.com>
2026-04-14 11:44:31 +0530

    Update exp7.py

--- exp7.py
+++ exp7.py
@@ -1,2 +1,11 @@
-for i in range(1,5):
-    print("*"*i)
+Grades = {"Alice":"A","Bob":"B"}
+Attendance = {"Alice":90, "Bob":85}
+Grades["Bob"] = "A"
+Attendance["Bob"] = 88
+Grades["Charlie"] = "C"
+Attendance["Charlie"] = 80
+Grades.pop("Alice")
+Attendance.pop("Alice")
+for Student in Grades:
+    print(Student,"-Grade",Grades[Student],"Attendance",Attendance[Student])
+    
